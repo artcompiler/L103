bdeb994583278b5d7e1e9b293d6e85189e52fd32
Jeff Dyer <jeffdyer@acm.org>
2018-09-21 15:00:11 -0600

    checkpoint

--- src/viewer.js
+++ src/viewer.js
@@ -1,6 +1,7 @@
 /* Copyright (c) 2016, Art Compiler LLC */
 import {assert, message, messages, reserveCodeRange} from "./assert";
 import * as React from "react";
+import * as d3 from "d3";
 
 window.gcexports.viewer = (function () {
   function capture(el) {
@@ -15,8 +16,8 @@ window.gcexports.viewer = (function () {
     } else if (score === 0) {
       rgb = "rgb(255, 100, 100)";
     } else {
-      rgb = "rgb(254, 243, 128)";
-//      rgb = "rgb(" + (255 * (1 - score)) + ", " + (255 * score) + "," + (100 * score) + ")";
+//      rgb = "rgb(254, 243, 128)";
+      rgb = "rgb(255, 100, 100)";
     }
     return rgb;
   }
@@ -36,8 +37,14 @@ window.gcexports.viewer = (function () {
       let x = spacing;
       let y = (2 * spacing + size) * i / 2;
       rgb = color(d.score / count);
-      elts.push(<rect key={i} x={x} y={y} width={size} height={size} fill={rgb} fillOpacity="1" strokeOpacity="0"/>);
-      elts.push(<text key={i+1} style={textStyle} x={x + size + spacing} y={y + size}>{input}</text>);
+//      elts.push(<rect key={i} x={x} y={y} width={size} height={size} fill={rgb} fillOpacity="1" strokeOpacity="0"/>);
+//      elts.push(<text key={i+1} style={textStyle} x={x /*+ size + spacing*/} y={y + size}>{input}</text>);
+      elts.push(<div className="mq" style={{
+        margin: "20 5 0 5",
+        border: "2px solid",
+        borderColor: rgb,
+        padding: 5,
+      }} key={i+1}>{d.input}</div>);
       i += 2;
       let offset = 0;
       Object.keys(d).forEach(k => {
@@ -47,24 +54,75 @@ window.gcexports.viewer = (function () {
         let v = d[k].value;
         let r = typeof d[k] === "boolean" && d[k] || d[k].result;
         offset++
-        let value = typeof d.symbolic.value === "string" && v || JSON.stringify(v);
-        let method = <tspan key="1">{ k + " " + (value || "")}</tspan>;
+        let value = typeof d.symbolic.value === "string" && <span key="2" className="mq">{v}</span> || <div key="2">{JSON.stringify(v)}</div>;
+//        let method = <tspan key="1">{ k + " " + (value || "")}</tspan>;
+        let method = <div key="1"><span key="1">{k}</span> {value}</div>;
         rgb = color(r && 1 || 0);
-        elts.push(<rect key={i} x={x + size + spacing }      y={y + offset * (size + spacing)} width={size} height={size} fill={rgb} fillOpacity="1" strokeOpacity="0"/>);
-        elts.push(<text key={i+1} style={textStyle} x={x + 2*size + 2*spacing} y={y + offset * (size + spacing) + spacing + spacing}>{method}</text>);
+        // elts.push(<rect key={i} x={x} y={y + offset * (size + spacing)} width={size} height={size} fill={rgb} fillOpacity="1" strokeOpacity="0"/>);
+        // elts.push(<text key={i+1} style={textStyle} x={x + size + spacing} y={y + offset * (size + spacing) + spacing + spacing}>{method}</text>);
+        //elts.push(<rect key={i} x={x} y={y + offset * (size + spacing)} width={size} height={size} fill={rgb} fillOpacity="1" strokeOpacity="0"/>);
+        elts.push(<div key={i} style={{
+          borderLeft: "2px solid",
+          borderColor: rgb,
+          padding: "0 5 0 5",
+          margin: 5,
+        }}>{method}</div>);
         i += 2;
       });
     });
     return elts;
   }
 
+  function loadScript(src, resume) {
+    var script = document.createElement("script");
+    script.onload = resume;
+    script.src = src;
+    script.type = "text/javascript";
+    document.getElementsByTagName("head")[0].appendChild(script);
+  }
+  function loadStyle(src, resume) {
+    var link = document.createElement("link");
+    link.onload = resume;
+    link.href = src;
+    link.rel = "stylesheet";
+    document.getElementsByTagName("head")[0].appendChild(link);
+  }
   var Viewer = React.createClass({
+    renderMath () {
+      if (window.MathQuill) {
+        d3.selectAll(".mq").each((v, i, e) => {
+          try {
+            let MQ = MathQuill.getInterface(2);
+            let mathQuill = MQ.StaticMath(e[i]);
+          } catch (x) {
+            console.log("ERROR rendering MathQuill: " + x);
+          }
+        });
+      } else if (window.MathJax) {
+        MathJax.Hub.Config({
+          tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]},
+          jax: ["input/TeX","output/SVG"],
+          extensions: ["tex2jax.js","MathMenu.js","MathZoom.js", "fast-preview.js", "AssistiveMML.js", "a11y/accessibility-menu.js"],
+          TeX: {
+            extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"]
+          }
+        });
+      }
+    },
     componentDidMount: function() {
+      // loadScript("https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js", () => {
+      // });
+      loadScript("/mathquill.js", () => {
+        loadStyle("/mathquill.css", () => {
+          this.componentDidUpdate();
+        });
+      });
     },
     componentDidUpdate: function() {
-      var bbox = $("svg g")[0].getBBox();
-      $("svg").attr("height", (bbox.height + 14) + "px");
-      $("svg").attr("width", (bbox.width + 14) + "px");
+      // var bbox = $("svg g")[0].getBBox();
+      // $("svg").attr("height", (bbox.height + 14) + "px");
+      // $("svg").attr("width", (bbox.width + 14) + "px");
+      this.renderMath();
     },
     render: function () {
       var props = this.props;
@@ -72,9 +130,9 @@ window.gcexports.viewer = (function () {
       var elts = render(data);
       return (
         <div>
-        <div className="L115">
+        <div className="L107">
         <div className="section">
-          <svg><g>{elts}</g></svg>
+          {elts}
         </div>
         </div>
         </div>
