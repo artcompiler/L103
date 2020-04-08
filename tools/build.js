0f17efef2b0aa2d7090595a5ea375df0259f7269
Megan Ly <megan@artcompiler.com>
2020-04-08 08:57:29 -0700

    Update rules to leave limits unevaluated

--- tools/build.js
+++ tools/build.js
@@ -3,7 +3,7 @@ import {execSync} from "child_process";
 
 // Current best rule sets
 const latexRulesID = "epLtg4YeVh5";
-const sympyRulesID = "p1Zt0bVNJCR";
+const sympyRulesID = "4LgCb2yvbTw";
 
 function rmdir(path) {
   try { var files = fs.readdirSync(path); }
