3a2fb967d89d3c59b15b1a6f0956f6dc1ffd61f3
Jeff Dyer <jeffdyer@acm.org>
2020-09-27 15:22:09 -0700

    Clean up viewer.jsx

--- src/viewer.jsx
+++ src/viewer.jsx
@@ -1,44 +1,17 @@
-/* Copyright (c) 2016, Art Compiler LLC */
+/* Copyright (c) 2020, ARTCOMPILER INC */
 import {assert, message, messages, reserveCodeRange} from "./assert";
 import * as React from "react";
 import * as d3 from "d3";
 import p5 from 'p5';
 
 window.gcexports.viewer = (function () {
-  const CLEAR = "#FEFEFE";
-  const YELLOW = "#E7B416";
-  const RED = "#CC3232";
-  const GREEN = "#2DC937";
-  function capture(el) {
-    var mySVG = $(el).html();
-    return mySVG;
-  }
-  function render(obj) {
-    let elts = [];
-    return elts;
-  }
-  function loadScript(src, resume) {
-    var script = document.createElement("script");
-    script.onload = resume;
-    script.src = src;
-    script.type = "text/javascript";
-    document.getElementsByTagName("head")[0].appendChild(script);
-  }
-  function loadStyle(src, resume) {
-    var link = document.createElement("link");
-    link.onload = resume;
-    link.href = src;
-    link.rel = "stylesheet";
-    document.getElementsByTagName("head")[0].appendChild(link);
-  }
   var Viewer = React.createClass({
     componentDidMount: function() {
       this.componentDidUpdate();
     },
     componentDidUpdate: function() {
-      d3.select('#sketch').html('');  // Clear any previous ink.
+      d3.select('#sketch').html(''); // Erase any existing ink.
       const setupBody = this.props.obj.join(';');
-      const drawBody = undefined;
       const fn = p => {
         p.setup = () => { new Function('p', setupBody)(p) };
         p.draw = () => new Function('p', '');
@@ -51,15 +24,13 @@ window.gcexports.viewer = (function () {
       return (
         <div>
         <div className="L102">
-        <div id="sketch" className="section">
-        </div>
+        <div id="sketch" className="section" />
         </div>
         </div>
       );
     },
   });
   return {
-    capture: capture,
     Viewer: Viewer
   };
 })();
