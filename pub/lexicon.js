e4a66ca515a9a09b5a1a53c5fea0a40e7b486cf9
Jeff Dyer <jeffdyer@acm.org>
2021-09-15 15:50:24 -0700

    Add pub dir

---
+++ pub/lexicon.js
@@ -0,0 +1,21 @@
+// The lexicon must be the last definition and the only one with a left curly.
+window.gcexports.globalLexicon = {
+  "sendPasscode" : { "tk": 1, "name": "SEND-PASSCODE", "cls": "function", "length": 1, "arity": 1 },
+  "getPasscode" : { "tk": 1, "name": "GET-PASSCODE", "cls": "function", "length": 1, "arity": 1 },
+  "goto" : { "tk": 1, "name": "GOTO", "cls": "function", "length": 1, "arity": 1 },
+  "button" : { "tk": 1, "name": "BUTTON", "cls": "function", "length": 2, "arity": 2 },
+  "input" : { "tk": 1, "name": "INPUT", "cls": "function", "length": 1, "arity": 1 },
+  "img" : { "tk": 1, "name": "IMG", "cls": "function", "length": 1, "arity": 1 },
+  "svg" : { "tk": 1, "name": "SVG", "cls": "function", "length": 2, "arity": 2 },
+  "path" : { "tk": 1, "name": "PATH", "cls": "function", "length": 2, "arity": 2 },
+  "code" : { "tk": 1, "name": "CODE", "cls": "function", "length": 2, "arity": 2 },
+  "a" : { "tk": 1, "name": "A", "cls": "function", "length": 2, "arity": 2 },
+  "ol" : { "tk": 1, "name": "OL", "cls": "function", "length": 2, "arity": 2 },
+  "ul" : { "tk": 1, "name": "UL", "cls": "function", "length": 2, "arity": 2 },
+  "li" : { "tk": 1, "name": "LI", "cls": "function", "length": 2, "arity": 2 },
+  "title" : { "tk": 1, "name": "TITLE", "cls": "function", "length": 2, "arity": 2 },
+  "span" : { "tk": 1, "name": "SPAN", "cls": "function", "length": 2, "arity": 2 },
+  "p" : { "tk": 1, "name": "P", "cls": "function", "length": 2, "arity": 2 },
+  "h3" : { "tk": 1, "name": "H3", "cls": "function", "length": 1, "arity": 1 },
+  "div" : { "tk": 1, "name": "DIV", "cls": "function", "length": 2, "arity": 2 }
+}
